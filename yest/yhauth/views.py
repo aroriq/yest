a750a6c03b1857ac01c0fc8472637710b3bf8d04
arori <hjmhjmop@gmail.com>
2021-09-05 16:26:24 +0900

    ユーザー登録機能

--- yest/yhauth/views.py
+++ yest/yhauth/views.py
@@ -6,19 +6,20 @@ from django.contrib.auth import authenticate, login
 # Create your views here.
 def signupview(request):
     if request.method == 'POST':
-        username_data = request.POST.get('username_data') #https://teratail.com/questions/230652
+        username_data = request.POST.get('username_data')
+        email_data = request.POST.get('email_data')
         password_data = request.POST.get('password_data')
         try:
-            user = User.objects.create_user(username_data, '', password_data)
+            user = User.objects.create_user(username_data, email_data, password_data)
         except IntegrityError:
             return render(request, 'signup.html', {'error':'このユーザー名は既に登録されています'})
     else:
         return render(request, 'signup.html', {})
-    return render(request, 'signup.html', {})
+    return render(request, 'signup.html', {'error':username_data+'さんのユーザー登録に成功しました'})
 
 def loginview(request):
     if request.method == 'POST':
-        username_data = request.POST.get('username_data') #https://teratail.com/questions/230652
+        username_data = request.POST.get('username_data')
         password_data = request.POST.get('password_data')
         user = authenticate(request, username=username_data, password=password_data)
         if user is not None:
