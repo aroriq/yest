4d494f779696694fa60dbcbe2fc81b168f26530a
arori <hjmhjmop@gmail.com>
2021-09-12 04:41:16 +0900

    #33 add_Model StaffModel,ContractModel,propertyModelの追加

--- yest/docs/admin.py
+++ yest/docs/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from .models import CustomerModel, CorpModel
+from .models import CustomerModel, CorpModel, StaffModel,PropertyModel, ContModel
 
 # Register your models here.
 admin.site.register(CustomerModel)
+admin.site.register(PropertyModel)
 admin.site.register(CorpModel)
+admin.site.register(StaffModel)
+admin.site.register(ContModel)
